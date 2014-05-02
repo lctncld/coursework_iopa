f169591027b5e7aa1f5d284dab5ee5e03bed3919
Andrey Volodin <lctncld@gmail.com>
2014-05-02 04:36:58 +0400

    Karnaught done

---
+++ karr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+
+input = sys.argv[1]
+
+texed = input[0] + input[4] + input[1] + input[5] + input[12] + input[8] + input[13] + input[9] + input[3] + input[7] + input[2] + input[6] + input[15] + input[11] + input[14] + input[10]
+
+print texed
